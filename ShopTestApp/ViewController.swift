d513afe5514fc33a9de3b520b96f1fa5962389c8
Stokefad <igor.naum.2013@yandex.ru>
2019-07-02 00:14:38 +0300

    Initial Commit

---
+++ ShopTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShopTestApp
+//
+//  Created by Igor-Macbook Pro on 02/07/2019.
+//  Copyright © 2019 Igor-Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
